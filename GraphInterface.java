03815bc0adb0468b49ddf9a02e0685272619cc1d
AigulKh <haku.kavai@gmail.com>
2016-06-26 23:44:37 +0300

    Add files via upload

--- GraphInterface.java
+++ GraphInterface.java
@@ -3,9 +3,4 @@ public interface GraphInterface {
 	public void clear();
 	public void addEdge(Integer x, Integer y);
 	public void input();
-	public static void main(String[] args) {
-		Graph i = new Graph();
-		i.input();
-		i.output();
-	}
 }
---
+++ Test.java
@@ -0,0 +1,8 @@
+
+public class Test {
+	public static void main(String[] args) {
+		Graph i = new Graph();
+		i.input();
+		i.output();
+	}
+}
