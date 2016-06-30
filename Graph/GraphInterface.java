8ef7399658a62d9177ff568fd116750f268fc75f
Elizaveta <karapusik_1995@mail.ru>
2016-06-30 18:46:26 +0300

    Изменен ввод

--- Graph/GraphInterface.java
+++ Graph/GraphInterface.java
@@ -1,7 +1,13 @@
+import java.util.ArrayList;
 
 public interface GraphInterface {
 	public void clear();
 	public void addEdge(Integer x, Integer y);
-	public void input();
-	public void output();
+	public void basic();
+	public void inputGraph();
+	public void outputGraph();
+	public boolean haveAnEdge(ArrayList<Integer> newNot, ArrayList<Integer> newCand);
+	public boolean edgeExist(int a, int b);
+	public void algBK(ArrayList<Integer> comsub, ArrayList<Integer> maxComsub,
+					  ArrayList<Integer> cand, ArrayList<Integer> not/*, int k*/);
 }
