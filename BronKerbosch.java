93ef4af519954de4efce7fe937699f64e913b984
Elizaveta <karapusik_1995@mail.ru>
2016-06-30 18:39:51 +0300

    Переделан ввод, переделан алгоритм

---
+++ BronKerbosch.java
@@ -0,0 +1,41 @@
+import java.util.*;
+public class BronKerbosch {
+   /* private static Set<Integer> compsub;
+    private static Set<Integer> candidates;
+    private static Set<Integer> not;
+    private static Set<Integer> max_compsub;
+    public static Set<Integer> GetBronKerbosch(Graph G, Set<Integer> candidates, Set<Integer> not)
+    {
+      for (int j=0; j<G.GetNumVertex(); j++)
+      {
+          candidates.add(j);
+      }
+
+        boolean check=true;
+        int v=0;
+        Set<Integer> noncontiguous;
+
+        while (candidates.isEmpty()&check)
+        {
+            while(!candidates.contains(v)){v++;}
+            compsub.add(v);
+            for (int j=0; j<G.GetNumVertex(); j++)
+            {
+                if ()
+            }
+            candidates.removeAll(noncontiguous);
+            not.removeAll(noncontiguous);
+            if (candidates.isEmpty()&not.isEmpty()) {
+                if (max_compsub.size() < compsub.size()) max_compsub = compsub;
+            }
+            else GetBronKerbosch(G, candidates, not);
+            compsub.remove(v);
+            candidates.remove(v);
+            not.add(v);
+        }
+
+        return max_compsub;
+    }*/
+
+    }
+
--- Test.java
+++ Test.java
@@ -4,10 +4,11 @@ import javax.swing.JFrame;
 public class Test {
 	public static void main(String[] args) {
 		GraphInterface i = new Graph();
-		i.input();
-		i.output();
+		//i.input();
+		i.outputGraph();
+		i.basic();
 		
-		Window app = new Window();
-	    app.setVisible(true);
+		/*Window app = new Window();
+	    app.setVisible(true);*/
 	}
 }
