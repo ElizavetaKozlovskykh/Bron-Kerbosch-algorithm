9b40ad53cac0b579dadefe2a2273056d13f2ef9f
AigulKh <haku.kavai@gmail.com>
2016-07-02 22:49:29 +0300

    bag

--- Window.java
+++ Window.java
@@ -1,6 +1,9 @@
 import java.awt.*;
 import java.awt.event.*;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 import javax.swing.*;
 
@@ -8,6 +11,8 @@ public class Window extends JFrame {
   private JLabel text1, text2;
   private JButton construct, start, next;
   private JPanel buttonsPanel;
+  private ArrayList<Integer> list1, list2, maxClique;
+  File f; Scanner scan;
 
   public Window() {
     super("Bron-Kerbosh algorithm");
@@ -18,7 +23,18 @@ public class Window extends JFrame {
     construct = new JButton("Ñonstruct a graph");
     start = new JButton("Start");
     next = new JButton("Next");
-
+    list1 = new ArrayList<>();
+    list2 = new ArrayList<>();
+    maxClique = new ArrayList<>();
+    
+    File f = new File("note.txt");
+	try {
+		Scanner scan = new Scanner(f);
+	} catch (FileNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+    
     add(text1, BorderLayout.NORTH); //ðàññòàâëÿåì êîìïîíåíòû ïî ìåñòàì;
     add(text2);
 
@@ -30,6 +46,20 @@ public class Window extends JFrame {
     setBounds(100, 100, 600, 300);
     setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
   }
+  private void print(ArrayList<Integer> list) {
+	  list.clear();
+	  int a, size;
+	  //File f = new File("note.txt");
+	  //Scanner scan = new Scanner(f);
+	  size = scan.nextInt();
+	  //if ( size != 0 )
+		  for ( int i = 0; i < size; i++ ) {
+			  a = scan.nextInt();
+			  list.add(a);
+		  };
+	  
+  }
+  private void paint() {}
   private void initListeners() {
 	  construct.addActionListener(new ActionListener() {
 		  public void actionPerformed(ActionEvent e) {
@@ -37,8 +67,8 @@ public class Window extends JFrame {
 			  start.setToolTipText("Click here to start the algorithm");
 			  buttonsPanel.add(start);
 			  revalidate(); repaint();
-			  text1.setText("Maximal clique: " );
-			  text2.setText("Ñurrent clique: " ); 
+			  text1.setText("Maximal clique: " + 0);
+			  text2.setText("Ñurrent clique: " + 0); 
 			  
 		  }
 	  });
@@ -48,16 +78,29 @@ public class Window extends JFrame {
 			  next.setToolTipText("Click here to continue search clique");
 			  buttonsPanel.add(next);
 			  revalidate(); repaint();
-			  text1.setText("Maximal clique: " );
-			  text2.setText("Ñurrent clique: " );
+			  text1.setText("Maximal clique: " + 0);
+			  text2.setText("Ñurrent clique: " + 0);
 		  }
 	  });
 	  next.addActionListener(new ActionListener() {
 		  public void actionPerformed(ActionEvent e) {
-			  //for ( ;; ) {
-			  text1.setText("Maximal clique: " );
-			  text2.setText("Ñurrent clique: " );
-			  //}
+			  do {
+				  print(list1); Integer a = list1.size();
+				  print(list2); Integer b = list2.size();
+				  if ( a.equals(b) ) {
+					  System.out.println("a: " + a + " b: " + b);
+					  maxClique.clear();
+					  maxClique.addAll(list2);
+				  }
+				  text2.setText("Ñurrent clique: " + list1.size() );
+				  text1.setText("Maximal clique: " + maxClique.size() );
+				  revalidate(); repaint();
+				  paint();
+			  } while ( !list2.isEmpty() );
+			  
+			  buttonsPanel.remove(next);
+			  revalidate(); repaint();
+			  text1.setText("The end" );
 		  }
 	  });
   }
