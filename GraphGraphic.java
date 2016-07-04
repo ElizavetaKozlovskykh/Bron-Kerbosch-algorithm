0e6c27a74fa3767ed0e6f17e9f0030c4f0576f84
Elizaveta <karapusik_1995@mail.ru>
2016-07-04 23:45:14 +0300

    Исправления  графики

--- GraphGraphic.java
+++ GraphGraphic.java
@@ -23,32 +23,36 @@ public class GraphGraphic extends JPanel  {
         check = false;
         clique = new ArrayList<>();
     }
+
+    public void SetCoordinate() {
+        Double alpha = 2 * Math.PI / gr.graph.numVert;
+        Double alpha0 = 0.0;
+        Integer R = 15 * gr.graph.numVert;
+        Integer x0 = 400;
+        Integer y0 = 365;
+        int a, b;
+        for ( int i = 0; i < gr.graph.numVert; i++ ) {
+            a =(int)Math.round(Math.cos(alpha0));
+            b=(int)Math.round(Math.sin(alpha0));
+            a= x0 + (int)(Math.round(Math.cos(alpha0) * R));
+        	b = y0 + (int)(Math.round(Math.sin(alpha0) * R));
+        	MyPoint tmp = new MyPoint(a, b);
+        	coordinates[i] = tmp;
+            alpha0 = alpha0 + alpha;
+        }
+    }
+    public void setClique(ArrayList<Integer> curClique, boolean check) {
+        clique.clear();
+        clique.addAll(curClique);
+        this.check = check;
+    }
     public void DrawVert(int x, int y, Graphics p, int num) {
         String str = Integer.toString(num);
         p.drawOval(x, y, 30, 30);
         p.drawString(str, x + 12, y + 20);
     }
-    public void SetCoordinate() {
-        Double alpha = 2 * Math.PI / gr.graph.numVert;
-        Double alpha0 = Math.PI;
-        Integer R = 20 * gr.graph.numVert;
-        Integer x0 = 400;
-        Integer y0 = 300;
-        for ( int i = 0; i < gr.graph.numVert; i++ ) {
-        	int a = x0 + (int)Math.round(Math.cos(alpha0)) * R; 
-        	int b = y0 + (int)Math.round(Math.sin(alpha0)) * R; 
-        	MyPoint tmp = new MyPoint(a, b);
-        	coordinates[i] = tmp;
-            alpha0 = alpha0 - alpha;
-        }
-    }
     public void DrawEdge(int x, int y, Graphics p) {
         p.drawLine(coordinates[x - 1].x + 25, coordinates[x - 1].y + 25, coordinates[y - 1].x + 25, coordinates[y - 1].y + 25);
-    }
-    public void setClique(ArrayList<Integer> curClique, boolean check) {
-    	clique.clear();
-    	clique.addAll(curClique);
-    	this.check = check;
     }
     public void paint(Graphics g) {
         SetCoordinate();
@@ -62,10 +66,10 @@ public class GraphGraphic extends JPanel  {
         }
         
         if ( check ) g.setColor(new Color(255, 0, 0));
-        else g.setColor(new Color(0, 100, 255));
+        else g.setColor(new Color(0, 0, 0));
 
         for ( int i = 0; i < clique.size(); i++ ) {
-            DrawVert(coordinates[i].x, coordinates[i].y, g, clique.get(i));
+            DrawVert(coordinates[clique.get(i)-1].x, coordinates[clique.get(i)-1].y, g, clique.get(i));
         }
     }
 }
